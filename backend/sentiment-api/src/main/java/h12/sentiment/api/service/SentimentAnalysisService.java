414a2ced6822c5e9d941be5b9aee5a317a46a14a
leandrofmoraes <leandrofmraes@gmail.com>
2025-12-10 18:00:22 -0300

    Método POST refeito

--- backend/sentiment-api/src/main/java/h12/sentiment/api/service/SentimentAnalysisService.java
+++ backend/sentiment-api/src/main/java/h12/sentiment/api/service/SentimentAnalysisService.java
@@ -2,10 +2,17 @@ package h12.sentiment.api.service;
 
 import org.springframework.stereotype.Service;
 
+import h12.sentiment.api.dto.InputSentimentDTO;
 import h12.sentiment.api.dto.OutputSentimentDTO;
+import h12.sentiment.api.model.SentimentAnalysis;
 
 @Service
 public class SentimentAnalysisService {
+
+  public SentimentAnalysis createAnalysis(InputSentimentDTO inputSentimentDTO) {
+    var sentimentAnalysis = new SentimentAnalysis(inputSentimentDTO);
+    return sentimentAnalysis;
+  }
 
   public OutputSentimentDTO getOneAnalysis() {
     return new OutputSentimentDTO("Positive", 0.95);
